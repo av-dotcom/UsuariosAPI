e22d4537c4abc1f26677073d3e0520a1d350cfac
av-dotcom <me+github@avilaca.com>
2024-12-19 10:48:08 -0300

    initial commit UsuariosAPI

--- obj/Debug/net9.0/UsuariosAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/UsuariosAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UsuariosAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05ca05b4e56598f150ca18415b8b6fb8d8caef94")]
 [assembly: System.Reflection.AssemblyProductAttribute("UsuariosAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UsuariosAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
